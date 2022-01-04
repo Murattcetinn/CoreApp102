2da7ba3bb4fb6b41cbba3665b2dca30ad9f5b47d
murattcetinn <muratcetin7@outlook.com>
2022-01-04 20:24:18 +0300

    Add project files.

---
+++ CoreApp102.Core/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using CoreApp102.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreApp102.Core.Services
+{
+    public interface IProductService:IService<Product>
+    {
+        Task<Product> GetWithByIdAsync(int proId);
+
+        //Product ile ilgili tanimlamam gerekene ic metodlarim var ise onlari bu servis katmaninda  yazabilirim.
+    }
+}
