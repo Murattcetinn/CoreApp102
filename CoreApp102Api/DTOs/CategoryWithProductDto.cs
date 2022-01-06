2ea2f4821960dcaea8d5c432968cb4d14155207c
murattcetinn <muratcetin7@outlook.com>
2022-01-06 20:24:44 +0300

    3. Day

--- CoreApp102Api/DTOs/CategoryWithProductDto.cs
+++ CoreApp102Api/DTOs/CategoryWithProductDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace CoreApp102.Api.DTOs
 {
     public class CategoryWithProductDto:CategoryDto
-    {
+    { 
         public IEnumerable<ProductDto> Products { get; set; }
     }
 }
